029304be099261c26e3c68e7a6e162a0b0cb3922
YoneyaDev <92635427+YoneyaDev@users.noreply.github.com>
2021-10-16 19:11:53 +0300

    Create ModulesLink.py

---
+++ ModulesLink.py
@@ -0,0 +1,37 @@
+# friendly-telegram
+
+import io, inspect
+from .. import loader, utils
+
+
+@loader.tds
+class ModulesLinkMod(loader.Module):
+    """Ссылка на модуль"""
+    strings = {'name': 'ModulesLink'}
+
+    async def mlcmd(self, message):
+        """Вывести ссылку на модуль"""
+        args = utils.get_args_raw(message)
+        if not args:
+            return await message.edit('<b>Нет аргументов.</b>')
+
+        await message.edit('<b>Поиск...</b>')
+
+        try:
+            f = ' '.join([x.strings["name"] for x in self.allmodules.modules if args.lower() == x.strings["name"].lower()])
+            r = inspect.getmodule(next(filter(lambda x: args.lower() == x.strings["name"].lower(), self.allmodules.modules)))
+
+            link = str(r).split('(')[1].split(')')[0]
+            if "http" not in link:
+                text = f"Модуль {f}:"
+            else:
+                text = f"<a href=\"{link}\">Ссылка</a> на {f}: <code>{link}</code>"
+
+            out = io.BytesIO(r.__loader__.data)
+            out.name = f + ".py"
+            out.seek(0)
+
+            await message.respond(text, file=out)
+            await message.delete()
+        except:
+            return await message.edit("<b>Модуля нет, либо вы ввели неверное название.</b>")
