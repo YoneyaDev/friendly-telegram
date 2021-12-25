6c3e1099941f399e652b69ff33357431e30d8acd
YoneyaDev <92635427+YoneyaDev@users.noreply.github.com>
2021-12-25 14:47:42 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # friendly-telegram
 
 echo "1. UserBot | Kajitsu_Yoneya > запуск..."
-cd ~/Telegram/KYoneya/ && screen -AmdS -r python3.10 -m friendly-telegram --no-web
+cd ~/Telegram/KYoneya/UserBot/ && screen -AmdS -r python3.10 -m friendly-telegram --no-web
 echo "2. UserBot | šveps > запуск..."
 cd ~/Telegram/Dima/UserBot/ && screen -AmdS -r python3.10 -m friendly-telegram --no-web
 echo "Ожидайте! Боты будут запущены в течении минуты!"
